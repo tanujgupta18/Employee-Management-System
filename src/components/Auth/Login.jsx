a3a9b9159b0258c53fbe97da0ab63ab3b5bda07d
Tanuj Gupta <tanujgupta1872003@gmail.com>
2025-12-05 17:04:12 +0530

    Changes

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,17 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("Form submitted");
+
+    setEmail("");
+    setPassword("");
+  };
+
   return (
     <div className="h-screen w-screen flex items-center justify-center">
       <div className="border-2 border-emerald-600 p-20 rounded-xl">
-        <form className="flex flex-col justify-center items-center">
+        <form
+          onSubmit={(e) => {
+            submitHandler(e);
+          }}
+          className="flex flex-col justify-center items-center"
+        >
           <input
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
             required
             className="outline-none bg-transparent border-2 border-emerald-600 font-medium text-lg py-2 px-6 rounded-full placeholder:text-gray-400"
             type="email"
             placeholder="Enter your email"
           />
           <input
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
             required
             className="outline-none bg-transparent border-2 border-emerald-600 font-medium text-lg py-2 px-6 rounded-full mt-3 placeholder:text-gray-400"
             type="password"
